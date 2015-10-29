10c69bba665a9fdf641539a2a45cbb22077b0936
Tom <tommil121@gmail.com>
2015-10-29 00:12:25 -0400

    I just started playing around with stuff.  Tried to make a create new league activity that currently looks like crap.  I'll work on it more tomorrow morning.

---
+++ app/src/androidTest/java/com/example/tom/fantasyfootballleague/MainActivityTest.java
@@ -0,0 +1,10 @@
+package com.example.tom.fantasyfootballleague;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by Tom on 10/28/2015.
+ */
+public class MainActivityTest extends TestCase {
+
+}
--- app/src/main/java/com/example/tom/fantasyfootballleague/MainActivity.java
+++ app/src/main/java/com/example/tom/fantasyfootballleague/MainActivity.java
@@ -8,13 +8,20 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "tomsMessage";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //helps to debug according to newbostonian video 9 - Activity States
+        Log.i(TAG, "onCreate");
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -28,6 +35,69 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    //------------------------------------------------
+    // the following code taken from newbostonian for
+    // debugging purposes
+    // From Video 10 - Running the State Changing Example
+    // Remove for an actual app
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(TAG, "onStart");
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(TAG, "onResume");
+    }
+
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(TAG, "onPause");
+    }
+
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(TAG, "onStop");
+    }
+
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i(TAG, "onRestart");
+    }
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(TAG, "onDestroy");
+    }
+
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        Log.i(TAG, "onSaveInstanceState");
+    }
+
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        Log.i(TAG, "onRestoreInstanceState");
+    }
+
+    // code from newbostonian ends here
+    //------------------------------------------
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
